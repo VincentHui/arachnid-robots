0b3ed2ec3fa5eeaf6b6ca9021f71e587475443a9
VincentHui <VincentHui@hotmail.co.uk>
2020-08-27 18:45:53 +0100

    Renaming and adding more tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,22 +2,25 @@ import { MK1, MK2, MK3 } from '../src/botLogic/bot';
 import { Vector3 } from '../src/botLogic/Vector3';
 
 describe('Robot Navigation', () => {
-  it('boundry test', () => {
+  it('boundry test for MK2', () => {
     const Bot = new MK2(new Vector3(0, 0), 'LLR');
     expect(Bot.position).toStrictEqual(new Vector3(1, 0, 0));
     expect(Bot.rotation).toStrictEqual(new Vector3(1, 0, 0));
   });
-  it('FFLRB cmd at position (0,0)', () => {
+
+  it('FFLRB cmd at position (0,0) for MK2', () => {
     const Bot = new MK2(new Vector3(0, 0), 'FFLRB');
     expect(Bot.position).toStrictEqual(new Vector3(1, 1, 0));
     expect(Bot.rotation).toStrictEqual(new Vector3(0, -1, 0));
   });
-  it('BBLL cmd at position (0,0) with no boundry checks', () => {
+
+  it('BBLL cmd at position (0,0) with no boundry checks for MK1', () => {
     const Bot = new MK1(new Vector3(0, 0), 'BBLL');
     expect(Bot.position).toStrictEqual(new Vector3(-2, -2, 0));
     expect(Bot.rotation).toStrictEqual(new Vector3(-1, 0, 0));
   });
-  it('Bot should rotate correctly', () => {
+
+  it('Bot should rotate correctly for MK1', () => {
     const Bot = new MK1();
     const nav: Vector3 = new Vector3(0, 1, 0);
     Bot.navigate(nav);
@@ -34,6 +37,7 @@ describe('Robot Navigation', () => {
   it('MK3 propulsion', () => {
     const Bot = new MK3(new Vector3(0, 0), 'FFF3L');
     expect(Bot.position).toStrictEqual(new Vector3(-1, 5, 0));
+    expect(Bot.rotation).toStrictEqual(new Vector3(-1, 0, 0));
     expect(Bot.fuel).toStrictEqual(24);
   });
 
