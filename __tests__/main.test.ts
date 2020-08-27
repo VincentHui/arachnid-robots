4eb1474f8f566c0fd3423b9e590d1b16b54e10ad
VincentHui <VincentHui@hotmail.co.uk>
2020-08-27 13:31:32 +0100

    using polymorphism for boundary checks

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,24 +1,24 @@
-import { Robot } from '../src/botLogic/bot';
+import { MK1, MK2 } from '../src/botLogic/bot';
 import { Vector3 } from '../src/botLogic/Vector3';
 
 describe('Robot Navigation', () => {
   it('boundry test', () => {
-    const Bot = new Robot(new Vector3(0, 0), 'LLR'.split(''));
+    const Bot = new MK2(new Vector3(0, 0), 'LLR'.split(''));
     expect(Bot.position).toStrictEqual(new Vector3(1, 0, 0));
     expect(Bot.rotation).toStrictEqual(new Vector3(1, 0, 0));
   });
   it('FFLRB cmd at position (0,0)', () => {
-    const Bot = new Robot(new Vector3(0, 0), 'FFLRB'.split(''));
+    const Bot = new MK2(new Vector3(0, 0), 'FFLRB'.split(''));
     expect(Bot.position).toStrictEqual(new Vector3(1, 1, 0));
     expect(Bot.rotation).toStrictEqual(new Vector3(0, -1, 0));
   });
   it('BBLL cmd at position (0,0) with no boundry checks', () => {
-    const Bot = new Robot(new Vector3(0, 0), 'BBLL'.split(''), false);
+    const Bot = new MK1(new Vector3(0, 0), 'BBLL'.split(''));
     expect(Bot.position).toStrictEqual(new Vector3(-2, -2, 0));
     expect(Bot.rotation).toStrictEqual(new Vector3(-1, 0, 0));
   });
   it('Bot should rotate correctly', () => {
-    const Bot = new Robot();
+    const Bot = new MK1();
     const nav: Vector3 = new Vector3(0, 1, 0);
     Bot.navigate(nav);
     nav.add(new Vector3(0, 0, 0));
--- src/botLogic/bot.ts
+++ src/botLogic/bot.ts
@@ -12,27 +12,47 @@ const CheckPos = (newPos: Vector3, oldPos: Vector3) => {
   return Check(newPos) ? newPos : oldPos;
 };
 
+const translateDirection = (
+  position: Vector3,
+  rotation: Vector3,
+  distance = 1,
+) => position.add(rotation.scalarMultiply(distance));
+
 export class Robot {
-  position: Vector3;
+  position: Vector3 = new Vector3();
   rotation: Vector3 = new Vector3(0, 1, 0);
-  willCheck = false;
-  constructor(
-    position: Vector3 = new Vector3(),
-    navSeq: string[] = [],
-    checkBoundary = true,
-  ) {
+
+  navigate(direction: Vector3, distance = 1): void {
+    this.rotation = direction;
+    this.position = translateDirection(this.position, this.rotation, distance);
+  }
+}
+
+export class MK1 extends Robot {
+  constructor(position: Vector3 = new Vector3(), navSeq: string[] = []) {
+    super();
     this.position = position;
-    this.willCheck = checkBoundary;
     navSeq.forEach((cmd) => {
       const cmdDirection = DirectionDispatch.get(cmd);
       if (!cmdDirection) return console.log('invalid cmd!');
       this.navigate(cmdDirection);
     });
   }
+}
 
+export class MK2 extends Robot {
+  constructor(position: Vector3 = new Vector3(), navSeq: string[] = []) {
+    super();
+    this.position = position;
+    navSeq.forEach((cmd) => {
+      const cmdDirection = DirectionDispatch.get(cmd);
+      if (!cmdDirection) return console.log('invalid cmd!');
+      this.navigate(cmdDirection);
+    });
+  }
   navigate(direction: Vector3, distance = 1): void {
     this.rotation = direction;
-    const newPos = this.position.add(direction.scalarMultiply(distance));
-    this.position = this.willCheck ? CheckPos(newPos, this.position) : newPos;
+    const newPos = translateDirection(this.position, this.rotation, distance);
+    this.position = CheckPos(newPos, this.position);
   }
 }
