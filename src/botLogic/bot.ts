9a283efbb2643ec2c3fae24371b038589b5584b7
VincentHui <VincentHui@hotmail.co.uk>
2020-08-27 18:45:30 +0100

    getting rid of uneeded CheckPos function

--- src/botLogic/bot.ts
+++ src/botLogic/bot.ts
@@ -8,10 +8,6 @@ const DirectionDispatch = new Map<string, Vector3>([
   ['R', new Vector3(1, 0, 0)],
 ]);
 
-const CheckPos = (newPos: Vector3, oldPos: Vector3) => {
-  return Check(newPos) ? newPos : oldPos;
-};
-
 const cmdsParser = (
   cmds: string,
   callback: (dir: Vector3, dist: number) => void,
@@ -61,7 +57,7 @@ export class MK2 extends Robot {
   navigate(direction: Vector3, distance = 1): void {
     this.rotation = direction;
     const newPos = this.position.translateDirection(this.rotation, distance);
-    this.position = CheckPos(newPos, this.position);
+    this.position = Check(newPos) ? newPos : this.position;
   }
 }
 
