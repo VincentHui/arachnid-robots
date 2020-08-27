2efb69f48bd611289782bd9b4b3d7a3e086db415
VincentHui <VincentHui@hotmail.co.uk>
2020-08-27 11:10:40 +0100

    Adding simple test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,31 +1,41 @@
-import { Delays, greeter } from '../src/main';
+// import { Delays, greeter } from '../src/main';
+import { Robot } from '../src/botLogic/bot';
+import { Vector3 } from '../src/botLogic/navigation';
 
-describe('greeter function', () => {
-  // Read more about fake timers
-  // http://facebook.github.io/jest/docs/en/timer-mocks.html#content
-  jest.useFakeTimers();
-
-  const name = 'John';
-  let hello: string;
-
-  // Act before assertions
-  beforeAll(async () => {
-    const p: Promise<string> = greeter(name);
-    jest.runOnlyPendingTimers();
-    hello = await p;
-  });
-
-  // Assert if setTimeout was called properly
-  it('delays the greeting by 2 seconds', () => {
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(
-      expect.any(Function),
-      Delays.Long,
-    );
-  });
-
-  // Assert greeter result
-  it('greets a user with `Hello, {name}` message', () => {
-    expect(hello).toBe(`Hello, ${name}`);
+describe('Robot Navigation', () => {
+  it('FFLRB cmd at position (0,0)', () => {
+    const Bot = new Robot(new Vector3(0, 0), 'FFLRB'.split(''));
+    console.log(Bot);
+    expect(true).toBe(true);
   });
 });
+
+// describe('greeter function', () => {
+//   // Read more about fake timers
+//   // http://facebook.github.io/jest/docs/en/timer-mocks.html#content
+//   jest.useFakeTimers();
+
+//   const name = 'John';
+//   let hello: string;
+
+//   // Act before assertions
+//   beforeAll(async () => {
+//     const p: Promise<string> = greeter(name);
+//     jest.runOnlyPendingTimers();
+//     hello = await p;
+//   });
+
+//   // Assert if setTimeout was called properly
+//   it('delays the greeting by 2 seconds', () => {
+//     expect(setTimeout).toHaveBeenCalledTimes(1);
+//     expect(setTimeout).toHaveBeenLastCalledWith(
+//       expect.any(Function),
+//       Delays.Long,
+//     );
+//   });
+
+//   // Assert greeter result
+//   it('greets a user with `Hello, {name}` message', () => {
+//     expect(hello).toBe(`Hello, ${name}`);
+//   });
+// });
